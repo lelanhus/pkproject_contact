2d09551803b4aed9ca1fdfcb9e40889cb650a15a
Leland Husband <leland.husband@gmail.com>
2011-03-25 14:10:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PkSignUp::Application.config.secret_token = '01d7d505c02196d391e15d574f8ff611bb7c1cad0a4c4625a649a2dac731556262f17f878289d29dfede3957c1011a093ccad7f23661a61920852453d5857ff2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PkSignUp::Application.config.session_store :cookie_store, :key => '_pk_sign_up_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PkSignUp::Application.config.session_store :active_record_store
